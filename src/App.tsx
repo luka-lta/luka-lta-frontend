6145e579abd746cd9924fcb51d1e38fd79f2b3b4
luka-lta <github.unselfish775@passmail.net>
2025-02-26 15:10:28 +0100

    Vercel added

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@ import {ThemeProvider} from "@/assets/providers/ThemeProvider.tsx";
 import {RouterProvider} from "react-router-dom";
 import {appRouter} from "@/AppRouter.tsx";
 import ErrorPage from "@/pages/ErrorPage.tsx";
+import {SpeedInsights} from "@vercel/speed-insights/react";
 
 function App() {
     return (
         <ThemeProvider defaultTheme='system' storageKey='ui-theme'>
             <RouterProvider router={appRouter} fallbackElement={<ErrorPage/>}/>
+            <SpeedInsights />
         </ThemeProvider>
     )
 }
