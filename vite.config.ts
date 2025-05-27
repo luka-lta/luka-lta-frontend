66ffbaa37ad76692fa333d9361e6834c37a00324
luka-lta <luka@luka-lta.dev>
2025-05-27 18:49:29 +0200

    #6 Google added

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,17 @@
 import path from "path"
 import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
+import {VitePluginRadar} from "vite-plugin-radar";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+      react(),
+      VitePluginRadar({
+        analytics: {
+          id: process.env.VITE_G_TAG ?? ''
+        }
+      }),
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
